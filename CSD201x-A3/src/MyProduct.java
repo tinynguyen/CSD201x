afbb54fcf20127834c6b21f1d6a86f602edbad44
TinyNguyen <ncd.ccpn@gmail.com>
2019-06-20 14:16:06 +0700

    Project 3 - Inventory management

---
+++ CSD201x-A3/src/MyProduct.java
@@ -0,0 +1,57 @@
+import entity.Product;
+import util.MyBSTree;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tiny
+ */
+public class MyProduct {
+
+  //a list of products
+  MyBSTree tree;
+
+  public MyProduct() {
+    tree = new MyBSTree();
+  }
+
+  //1.1 input and insert a new product to tree
+  public void insert() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+
+  //1.2 in-order traverse
+  public void inOrder() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+  //1.3 BFT a tree
+
+  public void BFT() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+  //1.4 search a product by product code
+
+  public void search() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+  //1.5 delete a product by product code
+
+  public void delete() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+  //1.6 simply balancing a tree
+
+  public void balance() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+  //1.7 count the number of products in the tree
+
+  public int size() {
+    throw new UnsupportedOperationException("Remove this line and implement your code here!");
+  }
+}
